eb3e5bb0ab01fcf9e243b6638ccb9dac0871dfd6
ringa-nyamvula <nyamvularinga@gmail.com>
2023-05-19 09:27:27 +0300

    Complete assignment

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,22 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+
+class EmailAddressParser
+    def initialize(emails)
+      @emails = emails
+    end
+
+    def parse
+      # Split the emails and remove any whitespace
+      parsed_emails = emails.split(/[, ]+/).map(&:strip)
+
+      # Remove duplicate emails
+      parsed_emails.uniq
+    end
+
+    private
+
+    attr_reader :emails
+  end
